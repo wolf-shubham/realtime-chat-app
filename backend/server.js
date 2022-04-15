22112560ee884cc79c12c5f9a28e207268630635
Shubham Sharma <sharma.shubham0191@gmail.com>
2022-04-15 23:45:37 +0530

    mongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
-
-const app = express()
-dotenv.config()
+const { databaseConnection } = require('./database/dbConnection')
 const userRoutes = require('./routes/userRoutes')
+
+
+dotenv.config({ path: '../.env' })
+const app = express()
+app.use(express.json())
+const PORT = process.env.PORT || 5000
 
 app.use('/user', userRoutes)
 
@@ -13,8 +17,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-
-const PORT = process.env.PORT || 5000
+databaseConnection()
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`)
