75549cf6c51c8ee5886335b7ef8418598d61bde3
Shubham Sharma <sharma.shubham0191@gmail.com>
2022-04-16 18:57:58 +0530

    registration frontend complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import Startup from './pages/Startup'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
+  const isAuthenticated = false
   return (
     <>
       <div className="App">
-        <Startup />
+        <Routes>
+          <Route path="/" element={isAuthenticated ? <Home /> : <Startup />} />
+        </Routes>
       </div>
     </>
   )
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,17 +1,61 @@
 import React, { useState } from 'react'
 import { Button, Checkbox, FormControlLabel, TextField } from '@material-ui/core'
+import axios from 'axios'
 
 
 const Register = () => {
 
+    const [name, setName] = useState('')
+    const [username, setUserName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        console.log(name, username, email, password)
+        try {
+            const config = {
+                headers: { 'Content-Type': 'application/json' }
+            }
+            const { data } = await axios.post('/user/register', {
+                name,
+                username,
+                email,
+                password
+            }, config)
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
 
     return (
         <>
             <div className="loginRight">
-                <form className='loginForm'>
+                <form className='loginForm' onSubmit={handleSubmit}>
                     <h1 className='titleLogin'>REGISTER</h1>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="name"
+                        label="Name"
+                        value={name}
+                        autoFocus
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="userName"
+                        label="Username"
+                        value={username}
+                        onChange={(e) => setUserName(e.target.value)}
+                    />
                     <TextField
                         variant="outlined"
                         margin="normal"
@@ -19,9 +63,7 @@ const Register = () => {
                         fullWidth
                         id="email"
                         label="Email Address"
-                        name="email"
                         value={email}
-                        autoFocus
                         onChange={(e) => setEmail(e.target.value)}
                     />
                     <TextField
